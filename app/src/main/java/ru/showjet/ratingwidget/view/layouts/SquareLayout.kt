6e3018fc75c5900cb33721977562c210f5805aee
Dmitry DGSC <dgsc.develop@gmail.com>
2019-03-26 18:10:11 +0300

    implementation with layout. a progressbar has padding. bad version

---
+++ app/src/main/java/ru/showjet/ratingwidget/view/layouts/SquareLayout.kt
@@ -0,0 +1,18 @@
+package ru.showjet.ratingwidget.view.layouts
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class SquareLayout : FrameLayout {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
